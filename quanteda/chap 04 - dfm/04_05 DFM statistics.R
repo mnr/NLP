390b03ab248646410fd8f782985db453ed50bc29
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-12-15 17:56:16 -0800

    initial commit

--- quanteda/chap 04 - dfm/04_05 DFM statistics.R
+++ quanteda/chap 04 - dfm/04_05 DFM statistics.R
@@ -4,3 +4,10 @@
 # dfm_tfidf
 # dfm_sample
 # docfreq
+
+ndoc(myDFM) # number of documents
+nfeat(myDFM) # number of features (aka terms)
+docnames(myDFM) # documents in DFM
+featnames(myDFM) # features in DFM
+
+topfeatures(myDFM, 5) # five most popular features (terms)
