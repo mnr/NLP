a6102ce284efdcea7bbb30b7aa513a1fd4d90a68
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-12-19 21:49:29 -0800

    don't need these

---
+++ quanteda/chap 04 - dfm/x 04_04 subset DFM.R
@@ -0,0 +1,7 @@
+# Subset and Select
+# dfm_select
+# dfm_remove
+# dfm_keep
+# dfm_replace
+# dfm_subset
+# dfm_trim
---
+++ quanteda/chap 04 - dfm/x 04_05 DFM statistics.R
@@ -0,0 +1,13 @@
+# Statistics
+# dfm_smooth
+# dfm_weight
+# dfm_tfidf
+# dfm_sample
+# docfreq
+
+ndoc(myDFM) # number of documents
+nfeat(myDFM) # number of features (aka terms)
+docnames(myDFM) # documents in DFM
+featnames(myDFM) # features in DFM
+
+topfeatures(myDFM, 5) # five most popular features (terms)
