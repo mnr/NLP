293d89021139d5113bf55bb959c3f1ac0d88eff4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-12-19 17:42:44 -0800

    tweaks

--- quanteda/chap 04 - dfm/04_02 create DFM.R
+++ quanteda/chap 04 - dfm/04_02 create DFM.R
@@ -14,18 +14,20 @@ scTokens <- tokens(corpus(data_corpus_inaugural),
                    remove_punct = TRUE,
                    remove_symbols = TRUE)
 
-scTokens <- tokens_remove(scTokens, pattern = stopwords("english"))
+scTokens <- tokens_remove(scTokens, pattern = stopwords(source = "smart"))
 
 # create a document-feature matrix
 dfm(scTokens) # what it looks like
 
 myDFM <- dfm(scTokens) # save the DFM for next steps
+save(myDFM, file = "quanteda/chap 04 - dfm/mydfm.rds")
 
+# dfm_sort
+sortedDFM <- dfm_sort(myDFM, margin = "features")
 
 # dfm_group
 # dfm_lookup
 # dfm_match
-# dfm_sort
 
 # rarely needed...
 # dfm_compress
