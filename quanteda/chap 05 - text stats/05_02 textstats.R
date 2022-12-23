c7a7894600385e9ea24a8f57eff4a746d3678b7d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-12-23 04:47:18 -0800

    add kwic

--- quanteda/chap 05 - text stats/05_02 textstats.R
+++ quanteda/chap 05 - text stats/05_02 textstats.R
@@ -9,3 +9,27 @@ sampleCorpus <- corpus(data_corpus_inaugural)
 readability <- textstat_readability(sampleCorpus)
 readability
 readability[order(readability$Flesch), ]
+
+# similarity and distance
+load(file = "quanteda/chap 04 - dfm/mydfm.rds")
+
+# show similiarity
+heatmap(
+  as.matrix(
+    textstat_simil(myDFM, margin = "documents")),
+  Rowv = NA, Colv = NA)
+
+# show dissimilarity (distance)
+heatmap(
+  as.matrix(
+    textstat_dist(myDFM, margin = "documents")),
+  Rowv = NA, Colv = NA)
+
+# summaries
+textstat_summary(sampleCorpus)
+
+# find often-used phrases
+textstat_collocations(sampleCorpus, size = 4)
+
+# where are keywords
+mykwic <- kwic(tokens(data_corpus_inaugural), pattern = "government")
