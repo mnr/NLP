c2ec8cfb7aeefd0d6a52aee880128db5078c5ea4
Mark Niemann-Ross <mark@niemannross.com>
2022-12-30 16:41:08 -0800

    better explanation of dfm_sort

--- quanteda/chap 04 - dfm/04_02 create DFM.R
+++ quanteda/chap 04 - dfm/04_02 create DFM.R
@@ -19,11 +19,20 @@ scTokens <- tokens_remove(scTokens, pattern = stopwords(source = "smart"))
 # create a document-feature matrix
 dfm(scTokens) # what it looks like
 
+# save as a dfm object
 myDFM <- dfm(scTokens) # save the DFM for next steps
-save(myDFM, file = "quanteda/chap 04 - dfm/mydfm.rds")
 
 # dfm_sort
-sortedDFM <- dfm_sort(myDFM, margin = "features")
+dfm_sort(dfm(scTokens[1]), margin = "features") #sort features of simplified example
 
+sortedDFM <- dfm_sort(myDFM, margin = "features") # sort features of all documents
+sortedDFM
 
+# frequency of all features
+head(featfreq(sortedDFM))
+# or...
+featfreq(dfm_select(sortedDFM, "people")) # appearances of "people"
+
+# save the dfm for use in next sessions
+save(myDFM, file = "quanteda/chap 04 - dfm/mydfm.rds")
 
