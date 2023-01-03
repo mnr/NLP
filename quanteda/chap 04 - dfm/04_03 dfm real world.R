e954958fb85a22b395c7c47ef7d5e201fcede529
Mark Niemann-Ross <mark@niemannross.com>
2023-01-02 16:05:55 -0800

    tweaks at recording

--- quanteda/chap 04 - dfm/04_03 dfm real world.R
+++ quanteda/chap 04 - dfm/04_03 dfm real world.R
@@ -20,7 +20,7 @@ names(billOfRights_dictionary) <- "BillOfRights"
 # examine Bill of Rights Dictionary
 billOfRights_dictionary
 
-# grab sentiment
+# stemming the 2015 Lexicoder Sentiment Dictionary
 LSD2015_stemmed <- dictionary(
                       as.list(
                         tokens_wordstem(
@@ -29,6 +29,7 @@ LSD2015_stemmed <- dictionary(
 
 LSD2015_stemmed
 
+# combine two dictionaries
 billOfRights_dictionary <- c(billOfRights_dictionary, LSD2015_stemmed)
 billOfRights_dictionary$neg_positive <- NULL
 billOfRights_dictionary$neg_negative <- NULL
@@ -44,19 +45,19 @@ dfm_lookup(x = myDFM_stemmed, dictionary = billOfRights_dictionary)
 dfm_trim(myDFM_stemmed, min_termfreq = 400)
 
 # look ahead to textstats
-head(textstat_frequency(myDFM_stemmed))
+# install.packages("quanteda.textstats")
+library(quanteda.textstats)
+head(textstat_frequency(myDFM_stemmed), 20)
 
-# subset features
-# list features starting with "free"
+# subset features starting with "free"
 dfm_select(myDFM_stemmed, pattern = startsWith(featnames(myDFM_stemmed), "free"))
 
-# subset documents
-# documents older than 1900
+# subset documents older than 1800
 dfm_subset(myDFM_stemmed,
            unlist(
              lapply(
               strsplit(docnames(myDFM_stemmed), split = "-"),
-              FUN = function(x, y) {x[1] < 1900}
+              FUN = function(x, y) {x[1] < 1800}
            )))
 # or...random documents
 dfm_sample(myDFM_stemmed) # random sample of documents
