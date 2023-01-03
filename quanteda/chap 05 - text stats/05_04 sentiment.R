cd9f096697633cdc328dad2b1fd05e00ec57a9de
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-01-02 18:08:14 -0800

    research

--- quanteda/chap 05 - text stats/05_04 sentiment.R
+++ quanteda/chap 05 - text stats/05_04 sentiment.R
@@ -8,9 +8,17 @@
 library(quanteda)
 library(quanteda.sentiment)
 
+# examples of dictionaries
+?data_dictionary_NRC # descriptions in help files
 dfm(tokens(data_dictionary_geninqposneg))
-dfm(tokens(data_dictionary_LoughranMcDonald))
-dfm(tokens(data_dictionary_NRC))
 
-temp <- data_dictionary_geninqposneg[2, "features" > 0]
-sample(unlist(temp), 10)
+# simple sentiment analysis
+inaugTokens <- tokens(data_corpus_inaugural, 
+                      remove_punct = TRUE,
+                      remove_symbols = TRUE,
+                      remove_numbers = TRUE)
+
+dfm(tokens_lookup(inaugTokens, dictionary = data_dictionary_NRC))
+dfm(tokens_lookup(inaugTokens, dictionary = data_dictionary_geninqposneg))
+
+# need to read https://rdrr.io/github/quanteda/quanteda.sentiment/f/vignettes/sentiment_analysis.Rmd
