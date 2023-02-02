a86be3e4579503d73754a47add32f0e0ebba9c5c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-02-02 14:24:26 -0800

    clean up and prep for recording

--- intro to nlp with tm/chap 03 - metadata/03-03 metadata find filter sort.R
+++ intro to nlp with tm/chap 03 - metadata/03-03 metadata find filter sort.R
@@ -1,10 +1,19 @@
 # create useful metadata
 library(tm, quietly = TRUE) 
-library(readtext)
 
+# retrieve corpus from previous session --------
 newVCorpus <- readRDS("newVCorpus.RDS")
 
-# use metadata to sort which documents to work on
+# Use base R subsetting: newVCorpus[1] shows the first element of newVCorpus
+newVCorpus[1]
+
+# Use base R subsetting: newVCorpus[[1]] shows the contents of the first element of newVCorpus")
+meta(newVCorpus[[1]])
+
+# tm addressing: type=local is equivalent to newVCorpus[[1]]
+meta(newVCorpus[1], type = "local")
+
+# How to use metadata to sort documents
 meta(newVCorpus, type = "corpus") # show corpus level metadata
 # ...then filter by specific metadata content
 meta(newVCorpus, type = "corpus", tag = "contributor") %in% "MNR"
