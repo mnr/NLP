4e5d7a831ebe3e5c208aaa07f42637bfadf224e6
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-02-09 17:22:35 -0800

    Clean up chapter 7

--- intro to nlp with tm/chap 07 - sentiment analysis/07-03 AFINN.R
+++
@@ -1,1 +0,0 @@
-# using the AFINN Lexicon
---
+++ intro to nlp with tm/chap 07 - sentiment analysis/07-03 sentiment datasets.R
@@ -0,0 +1,42 @@
+# sentiment datasets
+
+# install.packages("quanteda.textstats")
+library(quanteda.textstats)
+textstat_valence(x = smallText, dictionary = "nrc")
+
+# list the different dictionaries
+# AFINN - valence, one list
+data_dictionary_AFINN
+textstat_valence(x = "anger", 
+                 dictionary = data_dictionary_AFINN)
+
+# ANEW - valence, three lists
+data_dictionary_ANEW
+names(data_dictionary_ANEW)
+textstat_valence(x = "anger", 
+                 dictionary = data_dictionary_ANEW["pleasure"])
+textstat_valence(x = "anger", 
+                 dictionary = data_dictionary_ANEW["arousal"])
+textstat_valence(x = "anger", 
+                 dictionary = data_dictionary_ANEW["dominance"])
+
+# NRC - polarity
+data_dictionary_NRC
+names(data_dictionary_NRC)
+# what is the polarity?
+textstat_polarity(x = "anger", 
+                 dictionary = data_dictionary_NRC)
+# where is "anger" found?
+tokens_lookup(tokens("anger"), 
+              dictionary = data_dictionary_NRC)
+
+# browsing other dictionaries
+library(syuzhet)
+smallText <- get_tokens(c("hate","love","apathy"))
+
+get_nrc_sentiment(smallText)
+head(get_sentiment_dictionary(dictionary = "nrc"))
+tail(get_sentiment_dictionary(dictionary = "nrc"))
+head(get_sentiment_dictionary(dictionary = "bing"))
+head(get_sentiment_dictionary(dictionary = "afinn"))
+
