4354ecaa62324acb527431cc54fc3c470f30af3c
Mark Niemann-Ross <mark@niemannross.com>
2023-02-21 11:45:00 -0800

    remove data setup

--- intro to nlp with tm/chap 01 - intro/01-03 comparisons.R
+++ intro to nlp with tm/chap 01 - intro/01-03 comparisons.R
@@ -1,33 +1,15 @@
 # compare tm, quanteda, tidytext
 
-# retrieve sample data
-install.packages("gutenbergr")
-library(gutenbergr)
-
-SirMuhammadIqbal_meta <- gutenberg_works(author == "Iqbal, Muhammad, Sir")
-putFilesHere_name <- "SirMhumammadIqbal"
-dir.create(putFilesHere_name)
-setwd(putFilesHere_name)
-putFilesHere_dir <- getwd()
-for (eachWork in 1:nrow(SirMuhammadIqbal_meta)) {
-  print(SirMuhammadIqbal_meta[eachWork, "gutenberg_id"])
-  download.file(
-    paste0("https://gutenberg.org/files/", 
-           SirMuhammadIqbal_meta[eachWork, "gutenberg_id"],
-           "/",
-           SirMuhammadIqbal_meta[eachWork, "gutenberg_id"],
-           "-0.txt"),
-    destfile = paste0("SMI_", 
-                      SirMuhammadIqbal_meta[eachWork, "gutenberg_id"],
-                      ".txt")
-  )
-}
+# Note: you'll need to change the working directory
+# for example...
+# setwd("where is Muhammad_Iqbal?")
 
 # Base R with tm ---------
 library(tm)
 
 newSimpleCorpus <-
-  SimpleCorpus(DirSource(directory = putFilesHere_dir))
+  SimpleCorpus(DirSource(directory = "Muhammad_Iqbal",
+                         pattern = "./*txt"))
 
 newSimpleCorpus[[2]] <- removeNumbers(newSimpleCorpus[[2]])
 
@@ -40,12 +22,6 @@ summary(newSimpleCorpus)
 
 library(quanteda)
 library(readtext)
-
-textDF <- readtext(paste0(putFilesHere_dir, "/*"))
-
-
-textDF <- readtext("./Muhammad_Iqbal/*.txt",
-                   docvarsfrom = "filenames")
 
 textDF <- readtext("./Muhammad_Iqbal/*.txt",
                    docvarsfrom = "filenames")
@@ -62,7 +38,11 @@ summary(noNumQuantCorpus)
 library(tidytext)
 library(dplyr)
 
-newTidyNLP <- data.frame(readLines(list.files()[1])) %>%
+newTidyNLP <- list.files("Muhammad_Iqbal", pattern = "*txt")[1] %>%
+  readLines() 
+
+%>%
+  data.frame() %>%
   rename_with(.cols = 1, ~ "theText") %>%
   unnest_tokens(eachWord, theText) %>%
   filter(is.na(as.numeric(eachWord)))
