c3eeae1ffeecdf9edce1bf8de091b9b540959a0a
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-02-25 21:07:39 -0800

    remove acq

--- intro to nlp with tm/chap 04 - preprocessing/04-01 transformations.R
+++ intro to nlp with tm/chap 04 - preprocessing/04-01 transformations.R
@@ -1,28 +1,30 @@
-# Transformations
+# Transformations clean text
 library(tm)
-data("acq") # call up sample data from tm package
+
+# create a demonstration corpus ------------
+newVCorpus <- VCorpus(DirSource(directory = "poetry", pattern = "*.txt"))
 
 # list transformations available in tm ------
 getTransformations()
 
 # Remove whitespace with stripWhitespace ----
 # Before removing whitespace
-head(acq[[1]]$content)
+newVCorpus[[1]]$content
 
 # After removing whitespace
-head(stripWhitespace(acq[[1]]$content))
+stripWhitespace(newVCorpus[[1]]$content)
 
-# Remove numbers with removeNumbers
+# Remove numbers with removeNumbers
 # Before removing numbers
-head(acq[[1]]$content)
+head(newVCorpus[[1]]$content)
 
 # After removing numbers
-head(removeNumbers(acq[[1]]$content))
+removeNumbers(newVCorpus[[1]]$content)
 
 # Remove punctuation with removePunctuation
 
 # Before removing punctuation
-head(acq[[1]]$content)
+newVCorpus[[1]]$content
 
 # After removing punctuation
-head(removePunctuation(acq[[1]]$content))
+removePunctuation(newVCorpus[[1]]$content)
