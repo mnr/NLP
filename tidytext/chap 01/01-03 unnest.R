b9c3cec055eb77cd4ab54ec585a1bd8f47142ca7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-03-09 19:06:28 -0800

    finish code

--- tidytext/chap 01/01-03 unnest.R
+++ tidytext/chap 01/01-03 unnest.R
@@ -12,7 +12,7 @@ WizOzTibble %>% unnest_tokens(token = "words",
                               input = text,
                               output = "wizWords")
 
-# tokens don't have to be words
+# tokens don't have to be words -------
 
 WizOzTibble %>% unnest_tokens(token = "paragraphs",
                               input = text,
@@ -26,19 +26,34 @@ WizOzTibble %>% unnest_characters(input = text,
 WizOzTibble %>% unnest_lines(input = text,
                              output = "wizWords")
 
-# ngrams are combinations of words
+# ngrams are combinations of words ---------
 WizOzTibble %>% unnest_ngrams(input = text,
                               output = "wizWords",
                               n = 3)
+
+# character shingles are similar to ngrams
 WizOzTibble %>%
   unnest_character_shingles(input = text,
                             output = "wizWords",
                             n = 4)
+# skip_ngrams are similar to ngrams, but skip words
 WizOzTibble %>% unnest_skip_ngrams(input = text,
                                    output = "wizWords",
                                    n = 3,
                                    k = 2)
-# other speciality tokenizers
-WizOzTibble %>% unnest_ptb()
-WizOzTibble %>% unnest_regex()
+
+# other speciality tokenizers --------
+# penn treebank handles special cases
+# look at line 54 of ptbktokens
+ptbktokens <- WizOzTibble %>% unnest_ptb(input = text,
+                           output = "wizWords")
+# compare to line 52 & 53 of stdtokens
+stdtokens <- WizOzTibble %>% unnest_tokens(token = "words",
+                              input = text,
+                              output = "wizWords")
+# use regular expressions ----------
+# break on 2+ vowels (ebook) 
+WizOzTibble %>% unnest_regex(pattern = "[aeiou]{2,}",
+                             input = text,
+                             output = "wizWords")
 
