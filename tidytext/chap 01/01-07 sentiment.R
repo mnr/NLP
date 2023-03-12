88992c5110f84fdad944140441d45441d21039c7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-03-12 04:59:54 -0700

    initial commit

---
+++ tidytext/chap 01/01-07 sentiment.R
@@ -0,0 +1,30 @@
+# sentiment analysis
+
+library(tidyverse)
+library(tidytext)
+library(readtext)
+
+# sentiments is a dictionary of words ranked positive/negative
+sentiments
+unique(sentiments$sentiment)
+
+# select other sentiment dictionaries. afinn, nrc, bing, loughran
+get_sentiments(lexicon = "nrc")
+
+# use inner join to attach sentiment values
+sentimentsOZ <- readtext("WonderfulWizardofOz.txt") %>%
+  unnest_tokens(word, text) %>%
+  filter(!grepl('[[:digit:]]', word)) %>%
+  anti_join(stop_words, by = "word") %>%
+  inner_join(sentiments)
+
+# or...with nrc
+sentimentsOZ <- readtext("WonderfulWizardofOz.txt") %>%
+  unnest_tokens(word, text) %>%
+  filter(!grepl('[[:digit:]]', word)) %>%
+  anti_join(stop_words, by = "word") %>%
+  inner_join(get_sentiments(lexicon = "nrc"))
+
+# add this for count...
+%>%
+  count(sentiment, sort = TRUE)
