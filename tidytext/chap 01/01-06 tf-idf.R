156b90687b734efdd2df8bdea3e3404c4e5b713e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-03-11 17:38:57 -0800

    need multiple documents for tf*idf

--- tidytext/chap 01/01-06 tf-idf.R
+++ tidytext/chap 01/01-06 tf-idf.R
@@ -1,7 +1,32 @@
 # term frequency with bind_tf_idf( )
 
+library(tidyverse)
+library(tidytext)
+library(readtext)
+
+# standard term frequency ------------
 termFreq <- readtext("WonderfulWizardofOz.txt") %>%
   unnest_tokens(word, text) %>%
-  count(word)
+  filter(!grepl('[[:digit:]]', word)) %>%
+  anti_join(stop_words, by = "word") %>%
+  count(word, sort = TRUE) 
 
-bind_tf_idf( )
+# add this to calculate total words
+# %>%
+#  summarize(total = sum(n))
+
+# term frequency * inverse document frequency
+# tf Idf
+# this fails if there is only one document
+# so...idf = log(1/doc count) = 0
+# so tf*idf requires > 1 document
+
+tfidf_oz <- list.files(pattern = ".txt") %>%
+  readtext() %>%
+  unnest_tokens(word, text) %>%
+  count(doc_id, word, sort = TRUE) %>%
+  bind_tf_idf( term = word, document = doc_id, n = n ) %>%
+  arrange(desc(tf_idf))
+
+
+
