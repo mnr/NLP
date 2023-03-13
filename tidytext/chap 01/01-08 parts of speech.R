f7394e98d50d283e62fba733f189a4298f42423f
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-03-13 00:08:40 -0700

    example for parts of speech

--- tidytext/chap 01/01-08 parts of speech.R
+++ tidytext/chap 01/01-08 parts of speech.R
@@ -1,5 +1,9 @@
 # parts of speech
 
+library(tidyverse)
+library(tidytext)
+library(readtext)
+
 parts_of_speech
 unique(parts_of_speech$pos)
 
@@ -8,3 +12,33 @@ POS_OZ <- readtext("WonderfulWizardofOz.txt") %>%
   filter(!grepl('[[:digit:]]', word)) %>%
   anti_join(stop_words, by = "word") %>%
   inner_join(parts_of_speech)
+
+# Note: "Frank" vs "frank" --------
+POS_OZ <- readtext("WonderfulWizardofOz.txt") %>%
+  unnest_tokens(word, text, to_lower = FALSE) %>%
+  filter(!grepl('[[:digit:]]', word)) %>%
+  anti_join(stop_words, by = "word") %>%
+  inner_join(parts_of_speech)
+
+# what to do with this information? -------
+POS_OZ <- readtext("WonderfulWizardofOz.txt") %>%
+  unnest_tokens(word, text, to_lower = FALSE) %>%
+  inner_join(parts_of_speech) 
+
+getAWord <- function(typeOfWord) {
+  if (!(tolower(typeOfWord) %in% tolower(unique(POS_OZ$pos)))) {
+    return("WaT")
+  } else {
+    POS_OZ[POS_OZ$pos %in% typeOfWord, "word"] %>%
+      sample(size = 1) %>%
+      return()
+  }
+}
+
+getAWord(typeOfWord = "Noun")
+
+print(paste(getAWord(typeOfWord = "Pronoun"),
+            getAWord(typeOfWord = "Noun"),
+            getAWord(typeOfWord = "Adverb"),
+            getAWord(typeOfWord = "Verb (usu participle)")))
+
